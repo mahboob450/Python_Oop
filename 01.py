eb9e4e9a128f7dc14c923e36fb3f4af68b02d695
mdmahboobalam <mdmahboobalam450@gmail.com>
2025-12-24 09:16:43 +0530

    first commit

---
+++ 01.py
@@ -0,0 +1,8 @@
+# Procedure Oriented Approach (POA)
+# Procedure Oriented Approach is a programming paradigm where the program is designed around functions (procedures) rather than data.
+
+def Sum(a,b):
+    return a+b
+
+result=Sum(3,5)
+print(result)    
